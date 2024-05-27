7363b2d3cea843698f07b341185e35a3884f667a
Sintivrousai <116221123+Sintivrousai@users.noreply.github.com>
2024-05-27 17:47:13 +0300

    Add files via upload

--- Delivery.c
+++ Delivery.c
@@ -4,27 +4,31 @@
 #include <unistd.h>  
 #include <time.h>
 #include "constants.h" 
-#include <errno.h>
 
 int available_phones=Ntel;
 int available_cook = Ncook;
 int available_oven = Noven;
+int available_deliverer=Ndeliver;
 
-pthread_mutex_t phone_mutex, mutexCook, mutexOven;; 
-pthread_cond_t phone_cond, condCook, condOven;
+pthread_mutex_t phone_mutex, mutexCook, mutexOven, mutexDeliverer; 
+pthread_cond_t phone_cond, condCook, condOven, condDeliverer;
 
 int profit=0;
 int Sm=0;
 int Sp=0;
 int Ss=0;
 int successful_orders=0;
-int failed_orders=0; 
+int failed_orders=0;
+int sum_total_time=0;
+int max_time=0; 
+int sum_cold=0;
+int max_cold=0;
 
 void* phone_operator(void *threadId) {
     int *tid = (int *)threadId;
     unsigned int seed = time(NULL) ^ *tid;
-
-    struct timespec start_time, end_time;
+    //printf("%d\n", *tid);
+    struct timespec start_time, start_time_cold, end_time_prepare, end_time_deliver;
     clock_gettime(CLOCK_REALTIME, &start_time);
 
     pthread_mutex_lock(&phone_mutex);
@@ -105,30 +109,69 @@ void* phone_operator(void *threadId) {
     available_oven -= order_pizzas;
     pthread_mutex_unlock(&mutexOven);
 
-    sleep(Tbake * order_pizzas);
+    sleep(Tbake); 
+
     //printf("Pizzas ready\n");
+    clock_gettime(CLOCK_REALTIME, &start_time_cold);
 
     pthread_mutex_lock(&mutexOven);
     available_oven += order_pizzas;
     pthread_cond_signal(&condOven);
     pthread_mutex_unlock(&mutexOven);
+    
+    pthread_mutex_lock(&mutexDeliverer);
+    while (available_deliverer == 0) {
+        printf("No Deliverer Available\n");
+        pthread_cond_wait(&condDeliverer, &mutexDeliverer);
+    }
+    available_deliverer--;
+    pthread_mutex_unlock(&mutexDeliverer);
 
     pthread_mutex_lock(&mutexCook);
     available_cook++;
     pthread_cond_signal(&condCook);
     pthread_mutex_unlock(&mutexCook);
 
-     clock_gettime(CLOCK_REALTIME, &end_time);
-    int elapsed_time = (end_time.tv_sec - start_time.tv_sec) + 
-                          (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
+    clock_gettime(CLOCK_REALTIME, &end_time_prepare);
+    int elapsed_time_prepare = (end_time_prepare.tv_sec - start_time.tv_sec) + 
+                          (end_time_prepare.tv_nsec - start_time.tv_nsec) / 1e9;
 
-    printf("Η παραγγελία με αριθμό %d ετοιμάστηκε σε %d λεπτά.\n", *tid, elapsed_time );
+    printf("Η παραγγελία με αριθμό %d ετοιμάστηκε σε %d λεπτά.\n", *tid, elapsed_time_prepare );
+    
+    sleep(Tpack*order_pizzas);
+
+    int waiting=Tdellow + rand_r(&seed) % (Tdelhigh - Tdellow + 1);
+    sleep(waiting);
+    clock_gettime(CLOCK_REALTIME, &end_time_deliver);
+    int elapsed_time_delivery = (end_time_deliver.tv_sec - start_time.tv_sec) + 
+                      (end_time_deliver.tv_nsec - start_time.tv_nsec) / 1e9;
+    printf("Η παραγγελία με αριθμό %d παραδόθηκε σε %d λεπτά.\n", *tid, elapsed_time_delivery );
+    sum_total_time+=elapsed_time_delivery;
+    if (max_time<elapsed_time_delivery) {
+        max_time=elapsed_time_delivery;
+    }
+
+    int elapsed_time_cold = (end_time_deliver.tv_sec - start_time_cold.tv_sec) + 
+                      (end_time_deliver.tv_nsec - start_time_cold.tv_nsec) / 1e9;
+    sum_cold+=elapsed_time_cold;
+    if (max_cold<elapsed_time_cold) {
+        max_cold=elapsed_time_cold;
+    }
+    
+    available_deliverer +=1;
+    pthread_cond_signal(&condDeliverer);
+    pthread_mutex_unlock(&mutexDeliverer);
+
+    pthread_mutex_lock(&mutexOven);
+    available_oven += order_pizzas;
+    pthread_cond_signal(&condOven);
+    pthread_mutex_unlock(&mutexOven);
 
     return NULL;
 }
 
 int main(int argc, char *argv[]) { 
-  int Ncust = 10; 
+  int Ncust = 50; 
   pthread_t threads[Ncust]; 
   int threadIds[Ncust];
   int threadTime[Ncust]; 
@@ -174,5 +217,15 @@ printf("Όλες οι παραγγελίες έχουν ολοκληρωθεί.\
     printf("Συνολικές πίτσες Special: %d\n", Ss);
     printf("Πλήθος πετυχημένων παραγγελειών: %d\n", successful_orders);
     printf("Πλήθος αποτυχημένων παραγγελειών: %d\n", failed_orders); 
+
+    if (successful_orders>0) {
+        double average_time= sum_total_time/successful_orders;
+        printf("Ο μέσος χρόνος εξυπηρέτησης των πελατών είναι: %.2f \n", average_time); 
+        printf("Ο μέγιστος χρόνος εξυπηρέτησης των πελατών είναι: %d\n", max_time);
+
+        double average_cold_time= sum_cold/successful_orders;
+        printf("Ο μέσος χρόνος κρυώματος της παραγγελιας είναι: %.2f \n", average_cold_time); 
+        printf("Ο μέγιστος χρόνος κρυώματος της παραγγελιας είναι: %d\n", max_cold);
+    }
 return 0; 
 }  
